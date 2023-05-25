5cdb2fa0db9f8f6a6266d2f1a1a34588d39a4192
ljiang09 <ljiang@olin.edu>
2023-05-24 23:51:08 -0500

    add start of upload stamp input fields

--- src/components/upload/Upload.js
+++ src/components/upload/Upload.js
@@ -1,10 +1,54 @@
-import React from "react";
+import {React, useState} from "react";
+import {ToggleButton, ToggleButtonGroup} from '@mui/material';
 
-const Upload = () => (
-    <>
-        <div>this is the view for the Upload screen</div>
-    </>
-)
+
+function Upload() {
+    const [uploadType, setUploadType] = useState('single');
+
+    const handleUploadType = (event, newUploadType) => {
+        setUploadType(newUploadType);
+        // show either the single or set view
+    };    
+
+    return (
+        <>
+            <ToggleButtonGroup
+                value={uploadType}
+                exclusive
+                onChange={handleUploadType}
+            >
+                <ToggleButton value="single">
+                    Single
+                </ToggleButton>
+                <ToggleButton value="set">
+                    Set
+                </ToggleButton>
+            </ToggleButtonGroup>
+
+            <br />
+            <br />
+
+            {(uploadType === "single") ? (
+                <>
+                    <h2>Single</h2>
+                    <form>
+                        <label>Stamp name</label>
+                        <input type="text" />
+                        <br />
+                        <label>ncjksnj</label>
+                        <input type="text" />
+                        <br />
+                        <input type="submit" />
+                    </form>
+                </>
+            ) : (
+                <>
+                    <h2>Set</h2>
+                </>
+            )}
+        </>
+    );
+    }
 
 
 export default Upload;
