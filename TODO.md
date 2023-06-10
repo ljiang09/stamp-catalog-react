ab9ba5b60648c259e6f49b1c05fb7f77d78e0ad7
ljiang09 <ljiang@olin.edu>
2023-06-09 23:43:06 -0400

    update dependencies and todo

---
+++ TODO.md
@@ -0,0 +1,4 @@
+TODO:
+make tooltip background opaque
+add tags to each stamp, for easier filtering
+move styling to style.js files
--- src/components/catalog/Catalog.js
+++ src/components/catalog/Catalog.js
@@ -47,9 +47,3 @@ function Catalog() {
 }
 
 export default Catalog;
-
-
-// TODO:
-// make tooltip background opaque
-// add tags to each stamp, for easier filtering
-// move styling to style.js file
