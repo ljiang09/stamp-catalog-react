0557d4caebf65e942432dd04ebc320a35c9dfa6b
ljiang09 <ljiang@olin.edu>
2023-06-18 15:45:19 -0400

    store input values for upload single

--- src/components/upload/UploadOneStamp.js
+++ src/components/upload/UploadOneStamp.js
@@ -1,18 +1,32 @@
-import {React, useState} from "react";
+import {React, useState, useEffect} from "react";
 import {FormControl, TextField, Button, FormControlLabel, Checkbox} from '@mui/material';
 
 function UploadOneStamp() {
+    const [name, setName] = useState("");
+    const [value, setValue] = useState("");
+    const [date, setDate] = useState("");
+    const [description, setDescription] = useState("");
+    const [imgLink, setImgLink] = useState("");
+    const [owned, setOwned] = useState(false);
+
+    const handleClick = () => {
+        // check if any textfields are blank. if so, show error message
+        // if all good, submit to firebase
+    }
+
     return (
         <>
             <h2>Single</h2>
+            {/* TODO: add info tooltips for each input */}
             <FormControl>
-                <TextField size='small' label="Stamp Name" variant="outlined" style={{marginBottom: "10px"}} />
-                <TextField size='small' label="Value" variant="outlined" style={{marginBottom: "10px"}} />
-                <TextField size='small' label="Date" variant="outlined" style={{marginBottom: "10px"}} />
-                <TextField size='small' label="Description" variant="outlined" style={{marginBottom: "10px"}} />
-                <TextField size='small' label="Image Link" variant="outlined" style={{marginBottom: "10px"}} />
-                <FormControlLabel required control={<Checkbox />} label="Owned" />
-                <Button variant="outlined">Submit</Button>
+                <TextField size='small' value={name} onChange={(event) => setName(event.target.value)} label="Stamp Name" variant="outlined" style={{marginBottom: "10px"}} />
+                <TextField size='small' value={value} onChange={(event) => setValue(event.target.value)} label="Value" variant="outlined" style={{marginBottom: "10px"}} />
+                {/* TODO: replace the date input with actual MM DD YYYY type input? */}
+                <TextField size='small' value={date} onChange={(event) => setDate(event.target.value)} label="Date" variant="outlined" style={{marginBottom: "10px"}} />
+                <TextField size='small' value={description} onChange={(event) => setDescription(event.target.value)} label="Description" variant="outlined" style={{marginBottom: "10px"}} />
+                <TextField size='small' value={imgLink} onChange={(event) => setImgLink(event.target.value)} label="Image Link" variant="outlined" style={{marginBottom: "10px"}} />
+                <FormControlLabel checked={owned} onChange={(event) => setOwned(event.target.value)} required control={<Checkbox />} label="Owned" />
+                <Button variant="outlined" onClick={handleClick}>Submit</Button>
             </FormControl>
         </>
     );
