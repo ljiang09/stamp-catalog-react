b29e37b23cb0a5a8437e1f2e5735f27427c62361
ljiang09 <ljiang@olin.edu>
2023-06-18 15:03:55 -0400

    get + display stamps from firebase

--- src/components/catalog/Catalog.js
+++ src/components/catalog/Catalog.js
@@ -1,86 +1,98 @@
+import {Autocomplete, TextField} from "@mui/material";
 import {React, useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom"
-import { Autocomplete, TextField } from "@mui/material";
-import BinderImage from "./StampBinder.png";
+import {useNavigate} from "react-router-dom";
+
+import {retrieveCatalog} from "../../server/Firebase";
+
 import StampButton from "./components/StampButton";
-import {stampInfo, tagsList} from "./StampData";
-import useStyles from './styles.js'
+import BinderImage from "./StampBinder.png";
+import {tagsList} from "./StampData";
+import useStyles from "./styles.js";
 
 function Catalog() {
-    const [selectedFilters, setSelectedFilters] = useState([]);
-    const [displayedStamps, setDisplayedStamps] = useState(stampInfo);
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [displayedStamps, setDisplayedStamps] = useState({});
+  const [stampInfo, setStampInfo] = useState({});
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    useEffect(() => {
-        // set the displayedStamps based on the selected filters
-        if (selectedFilters.length === 0) {
-            setDisplayedStamps(stampInfo);
-        } else {
-            const newDisplayedStamps = Object.entries(stampInfo.sets).reduce((acc, [key, value]) => {
-                const hasMatchingTags = value.tags.some(tag => selectedFilters.includes(tag));
-                if (hasMatchingTags) {
-                  acc[key] = value;
-                }
-                return acc;
-              }, {});
-            const obj = {
-                sets: newDisplayedStamps
+  // retrieve data from firebase upon loading
+  useEffect(() => {
+    console.log("Retrieving catalog...");
+    retrieveCatalog(setStampInfo);
+  }, []);
+
+  useEffect(() => {
+    console.log("all stamps", stampInfo);
+  }, [stampInfo]);
+
+  useEffect(() => {
+    // set the displayedStamps based on the selected filters
+    if (selectedFilters.length === 0) {
+      setDisplayedStamps(stampInfo);
+    } else {
+      const newDisplayedStamps =
+          Object.entries(stampInfo.sets).reduce((acc, [key, value]) => {
+            const hasMatchingTags =
+                value.tags.some(tag => selectedFilters.includes(tag));
+            if (hasMatchingTags) {
+              acc[key] = value;
             }
-            setDisplayedStamps(obj);
-        }
-    }, [selectedFilters]);
+            return acc;
+          }, {});
+      const obj = {sets: newDisplayedStamps};
+      setDisplayedStamps(obj);
+    }
+  }, [stampInfo, selectedFilters]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClick = (setID, stampObj) => {
-        // sets will have a "stamps" key
-        if ("stamps" in stampObj) {
-            navigate(`/info/${setID}`);
-        } else {
-            // if set, use "setID". if single, use basic ID
-            console.log("Single stamp clicked");
-        }
-    };
+  const handleClick = (setID, stampObj) => {
+    // sets will have a "stamps" key
+    if ("stamps" in stampObj) {
+      navigate(`/info/${setID}`);
+    } else {
+      // if set, use "setID". if single, use basic ID
+      console.log("Single stamp clicked");
+    }
+  };
 
     return (
-        <>
-            <h2>Lily's Stamps</h2>
+      <>
+        <h2>Lily's Stamps</h2>
+        {/* need the selected parameters to update the thing */}
+        <Autocomplete
+            multiple
+            options={tagsList}
+            onChange={(event, value) => setSelectedFilters(value)}
+            renderInput={(params) => <TextField {...params} label="Filter by:" />}
+            className={classes.filterBy}
+        />
 
-{/* need the selected parameters to update the thing */}
-            <Autocomplete
-                multiple
-                options={tagsList}
-                onChange={(event, value) => setSelectedFilters(value)}
-                renderInput={(params) => <TextField {...params} label="Filter by:" />}
-                className={classes.filterBy}
-            />
+        <p>Sort by: All (other options: sets, singles, full sets, owned, clocks, christmas, other categories)</p>
 
-            <p>Sort by: All (other options: sets, singles, full sets, owned, clocks, christmas, other categories)</p>
+        <div style={{height: "0.5vw"}}></div>
 
-            <div style={{height: "0.5vw"}}></div>
+        <img src={BinderImage} alt="stamp binder page" className={classes.binderBackground} />
 
-            <img src={BinderImage} alt="stamp binder page" className={classes.binderBackground} />
+        <div style={{height: "0.5vw"}}></div>
 
-            <div style={{height: "0.5vw"}}></div>
-
-            {Object.entries(displayedStamps.sets).map(([key, value]) => (
-                <div className={classes.stampRow}>
-                    {value.stamps.map((obj) => (
-                        <StampButton
-                            key={obj.id}
-                            obj={obj}
-                            setName={value.setName}
-                            tags={value.tags}
-                            alt=""
-                            handleClick={() => handleClick(key, value)}
-                        />
-                    ))}
-                </div>
+        {displayedStamps.hasOwnProperty("sets") && Object.entries(displayedStamps.sets).map(([key, value]) => (
+          <div className={classes.stampRow}>
+            {value.stamps.map((obj) => (
+              <StampButton
+                key={obj.id}
+                obj={obj}
+                setName={value.setName}
+                tags={value.tags}
+                alt=""
+                handleClick={() => handleClick(key, value)}
+              />
             ))}
-
-        </>
+          </div>
+        ))}
+      </>
     )
-}
+  }
 
-export default Catalog;
+  export default Catalog;
---
+++ src/server/Firebase.js
@@ -0,0 +1,57 @@
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js';
+import {get, getDatabase, ref as ref_database, set} from 'https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js';
+import {getDownloadURL, getStorage, ref as ref_storage, uploadBytesResumable} from 'https://www.gstatic.com/firebasejs/9.16.0/firebase-storage.js';
+import {useState} from 'react';
+
+import {stampInfo} from '../components/catalog/StampData';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDS3xYZstK58Y8DgpHR8HCyKlJXi3N41cs',
+  authDomain: 'stamp-catalogue-6df49.firebaseapp.com',
+  databaseURL: 'https://stamp-catalogue-6df49-default-rtdb.firebaseio.com',
+  projectId: 'stamp-catalogue-6df49',
+  storageBucket: 'stamp-catalogue-6df49.appspot.com',
+  messagingSenderId: '74536590288',
+  appId: '1:74536590288:web:ab55c95d93d24baca8f7d5',
+  measurementId: 'G-3NVTTQ81P0'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage();
+const db = getDatabase();
+
+console.log('firebase initialized');
+
+// used for beginning testing to generate some basic data
+const initialPost = () => {
+  const jsonData = JSON.stringify(stampInfo);
+
+  set(ref_database(db, 'stampInfo'), JSON.parse(jsonData))
+      .then(function() {
+        window.alert('Uploaded!');
+      })
+      .catch(function(error) {
+        console.log('Synchronization failed', error);
+      });
+};
+
+
+const retrieveCatalog = (setAllStamps) => {
+  // TODO: load sets, then singles
+
+  get(ref_database(db, 'stampInfo/'))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          setAllStamps(snapshot.val());
+          console.log('Catalog retrieved');
+        } else {
+          console.log('No data available at location `stampInfo/`');
+        }
+      })
+      .catch((error) => {
+        console.error('error with loading stamp info', error);
+      });
+};
+
+export {initialPost, retrieveCatalog};
