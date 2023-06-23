70f6fc31f750114d323de8566df3c688f0e9896a
ljiang09 <ljiang@olin.edu>
2023-06-22 22:19:46 -0500

    fix component size and code styling

--- src/components/upload/InputField.js
+++ src/components/upload/InputField.js
@@ -1,64 +1,65 @@
 import { TextField, IconButton, Tooltip } from "@mui/material";
-import InfoIcon from '@mui/icons-material/Info';
+import InfoIcon from "@mui/icons-material/Info";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles({
-    root: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "10px"
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "15px",
+  },
+  error: {
+    "& label.Mui-focused": {
+      color: "red",
     },
-    error: {
-        '& label.Mui-focused': {
-            color: 'red',
-        },
-        '& .MuiInput-underline:after': {
-            borderBottomColor: 'red',
-        },
-        '& .MuiOutlinedInput-root': {
-            '& fieldset': {
-            borderColor: 'red',
-            },
-            '&:hover fieldset': {
-            borderColor: 'red',
-            },
-            '&.Mui-focused fieldset': {
-            borderColor: 'red',
-            },
-        }
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "red",
     },
-    tooltip: {
-        width: "100px"
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "red",
+      },
+      "&:hover fieldset": {
+        borderColor: "red",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "red",
+      },
     },
-    iconBtn: {
-        width: "20px",
-        height: "20px"
-    }
+  },
+  tooltip: {
+    width: "100px",
+  },
+  iconBtn: {
+    width: "20px",
+    height: "20px",
+  },
 });
 
 // TODO: make the textfield outlined in red with an error message below if errorHandler changes
-const InputField = ({value, handleChange, label, info, error, errorMsg}) => {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <TextField
-                size='small'
-                value={value}
-                onChange={handleChange}
-                label={label}
-                helperText={errorMsg}
-                variant="outlined"
-                className={error && classes.error}
-                sx={{ mr: 1 }}
-            />
-            <Tooltip title={info} arrow placement="right" className={classes.tooltip}>
+const InputField = ({ value, handleChange, label, info, error, errorMsg }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <TextField
+        size="small"
+        value={value}
+        onChange={handleChange}
+        label={label}
+        helperText={errorMsg}
+        variant="outlined"
+        className={error && classes.error}
+        style={{ width: "200px" }}
+        sx={{ mr: 1 }}
+      />
+      {/* <Tooltip title={info} arrow placement="right" className={classes.tooltip}>
                 <IconButton className={classes.iconBtn}>
                     <InfoIcon />
                 </IconButton>
-            </Tooltip>
-        </div>
-    );
+            </Tooltip> */}
+    </div>
+  );
 };
 
 export default InputField;
