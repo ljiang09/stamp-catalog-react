8e2c35a22fa751ebe820ab79b6e5ce26380b653a
ljiang09 <ljiang@olin.edu>
2023-07-01 01:58:40 -0400

    show real data in stamp settings table

--- src/components/settings/components/StampsTable/index.js
+++ src/components/settings/components/StampsTable/index.js
@@ -1,4 +1,4 @@
-import { React } from "react";
+import { React, useState, useEffect } from "react";
 import {
   TableContainer,
   Paper,
@@ -9,8 +9,25 @@ import {
   Table,
   Button,
 } from "@mui/material";
+import { retrieveCatalog, retrieveTags } from "../../../../server/Firebase.js";
 
 function StampsTable() {
+  const [stampInfo, setStampInfo] = useState({});
+  const [tags, setTags] = useState([]);
+
+  // this is duplicate code. move this to init eventually
+  useEffect(() => {
+    retrieveCatalog(setStampInfo);
+    retrieveTags((value) => {
+      setTags(value);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log("stamp info:", stampInfo);
+  }, [stampInfo]);
+  // TODO: create updating functions in firebase
+
   // TODO: figure out if this should better be in tree format
   return (
     <TableContainer component={Paper} style={{ width: "70%", margin: "auto" }}>
@@ -20,7 +37,6 @@ function StampsTable() {
             <TableCell>Stamp Name</TableCell>
             <TableCell>Date</TableCell>
             <TableCell>Value</TableCell>
-            <TableCell>Description</TableCell>
             <TableCell>Height</TableCell>
             <TableCell>Image</TableCell>
             <TableCell>Owned</TableCell>
@@ -29,20 +45,30 @@ function StampsTable() {
           </TableRow>
         </TableHead>
         <TableBody>
+          {stampInfo.hasOwnProperty("sets") &&
+            Object.entries(stampInfo.sets).map(([key, value]) => {
+              return value.stamps.map((obj) => (
+                <TableRow key={key}>
+                  <TableCell>
+                    {obj.description ? obj.description : "N/A"}
+                  </TableCell>
+                  <TableCell>{obj.date ? obj.date : "N/A"}</TableCell>
+                  <TableCell>{obj.value ? obj.value : "N/A"}</TableCell>
+                  <TableCell>{obj.height ? obj.height : "N/A"}</TableCell>
+                  <TableCell>idk how to show img</TableCell>{" "}
+                  <TableCell>{obj.owned ? "true" : "false"}</TableCell>
+                  <TableCell>
+                    {value.setName ? value.setName : "N/A, or single"}
+                  </TableCell>
+                  <TableCell>
+                    <Button onClick={() => console.log("editing")}>Edit</Button>
+                  </TableCell>
+                </TableRow>
+              ));
+            })}
           {/* TODO: add map function here */}
-          <TableRow>
-            <TableCell>Stamp Name</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Value</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Height</TableCell>
-            <TableCell>Image</TableCell>
-            <TableCell>Owned</TableCell>
-            <TableCell>Set Name (N/A if single)</TableCell>
-            <TableCell>
-              <Button onClick={() => console.log("editing")}>Edit</Button>
-            </TableCell>
-          </TableRow>
+
+          {/* TODO: map the singles too */}
         </TableBody>
       </Table>
     </TableContainer>
